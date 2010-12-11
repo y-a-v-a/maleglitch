3784d768999c303e97d9873fb755278151520a77
Vincent Bruijn <vebruijn@gmail.com>
2010-12-11 10:22:20 +0100

    changed filenaming and added html index

---
+++ img.php
@@ -0,0 +1,13 @@
+<?php
+
+// Maleglitch
+require 'Satromizer.php';
+// error_reporting(E_ALL);
+
+// $s = new Satromizer('IMG_0007.JPG');
+// $s->show();
+header('Content-type: image/jpeg');
+$s = new Satromizer('src/malevich.00'.rand(1,6).'.jpg');
+echo $s->show();
+$s->export('imgs/');
+exit;
--- index.php
+++ index.php
@@ -1,11 +1,39 @@
-<?php
-require 'Satromizer.php';
-// error_reporting(E_ALL);
+<?php header("Content-Type: text/html; charset=UTF-8"); ?>
+<!DOCTYPE html>
 
-// $s = new Satromizer('IMG_0007.JPG');
-// $s->show();
-header('Content-type: image/jpeg');
-$s = new Satromizer('IMG_0085.JPG');
-echo $s->show();
-$s->export('imgs/');
-exit;
+<html lang="en">
+
+<head class="html5reset-bare-bones">
+
+	<meta charset="utf-8">
+
+	<!--[if IE]><![endif]-->
+	
+	<title>Maleglitch</title>
+	
+	<meta name="description" content="">
+
+	<!--link rel="stylesheet" href="_/css/main.css">
+	<link rel="stylesheet" href="_/css/_print/main.css" media="print"-->
+	
+</head>
+
+<body>
+
+<div style="text-align: center">
+
+	<header>
+	
+		<h1>Maleglitch</h1>
+		
+	
+	</header>
+	
+	<div><img src="img.php" alt="" style="height: 500px"></div>
+	
+	<footer>yava</footer>
+
+</div>
+
+</body>
+</html>
