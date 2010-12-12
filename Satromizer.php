ad00b7164acc89d607e76e4e8918949a536105f0
Vincent Bruijn <vebruijn@gmail.com>
2010-12-12 11:31:34 +0100

    refactored class and class behaviour

--- Satromizer.php
+++ Satromizer.php
@@ -16,6 +16,8 @@ class Satromizer {
 	
 	private $def_img = null;
 	
+	private $parts;
+	
 	public function __construct($file = '') {
 		$this->orig_img = @file_get_contents($file);
 		if ($this->orig_img == false) {
@@ -24,69 +26,57 @@ class Satromizer {
 		return $this;
 	}
 	
+	/**
+	 * @return resource image
+	 */
 	public function show() {
-		// trick to strip all kinds of headers from a jpeg file
-		$img = @imagecreatefromstring($this->orig_img);
-		ob_start();
-		// header('Content-type: image/jpeg');
-	    imagejpeg($img, null, 80);
-		// die;
-	    $i = ob_get_clean();
+		header('Content-type: image/jpeg');
+		return $this->def_img;
+	}
+	
+	/**
+	 * Build a glitched image
+	 * @return object $this
+	 */
+	public function build() {
+		$this->tmp_img = $this->stripHeaders();
+		$this->parts = $this->chunkValues();
 
-		$this->tmp_img = $i;
-		$img_size = strlen($this->tmp_img);
-		
-		$parts = $this->chunkValues($img_size);
-		$parts_tot = count($parts);
-		
-		// chop original image in pieces
-		$tmp = array();
-		while(!is_null($key = key($parts))) {
-			$curr = current($parts);
-			$next = next($parts);
-			$tmp[] = substr($this->tmp_img, $curr, $next - $curr);
-			if ($next == $parts[$parts_tot - 1]) {
-				break;
-			}
-		}
-
-		// create some addition / repetitive image data
-		$additions = array();
-		for ($i = 1; $i < $this->chunks; $i++) {
-			$rs = array();
-			for($j = 0; $j < 2; $j++) {
-				$rs[] = rand($this->chunk_size_min, $this->chunk_size_max);
-			}
-			sort($rs);
-			$h = $img_size / rand(2,4);
-			$add = substr($this->tmp_img,$h - $rs[0], $h - $rs[1]);
-			$additions[] = $add;
-		}
+		$image_parts = $this->getImageInParts();
+		$additions = $this->getAdditions();
 
 		// merge arrays
-		$arr = array();
-		foreach($tmp as $k=>$t) {
-			$arr[] = $t;
+		$arr = array();$i = 0;
+		foreach($image_parts as $k => $t) {
+			$arr[$i++] = $t;
 			if (isset($additions[$k])) {
-				$arr[] = $additions[$k];
+				$arr[$i++] = $additions[$k];
 			}
 		}
 		// create one big string
 		$tmp = implode('',$arr);
 
 		$this->def_img = @imagecreatefromstring($tmp);
+		unset($tmp);
+		if ($this->def_img === false) {
+			$this->def_img = @imagecreatetruecolor(300,300);
+		}
 		// header('Content-type: image/jpeg');
 		
 		ob_start();
-		imagejpeg($this->def_img, null, 50);
+		$is_img = @imagejpeg($this->def_img, null, 50);
+		if ($is_img == false) {
+			throw new Exception('Cannot create an image.');
+		}
 		$this->def_img = ob_get_clean();
 		// exit;
-		return $this->def_img;
+		return $this;
 	}
 	
 	/**
 	 * Export satromized image to jpg file
 	 * @param string directory to write to.
+	 * @return object $this
 	 */
 	public function export($target = '') {
 		if ($this->def_img != null) {
@@ -95,6 +85,57 @@ class Satromizer {
 				throw new Exception('Cannot write image. Permissions issue?');
 			}
 		}
+		return $this;
+	}
+	
+	/**
+	 * @return resource image
+	 */
+	private function stripHeaders() {
+		// trick to strip all kinds of headers from a jpeg file
+		$img = @imagecreatefromstring($this->orig_img);
+		ob_start();
+	    $is_img = @imagejpeg($img, null, 80);
+		if ($is_img === false) {
+			throw new Exception('Cannot strip headers from image.');
+		}
+	    $img_buffer = ob_get_clean();
+		imagedestroy($img);
+		return $img_buffer;
+	}
+	
+	/**
+	 * Chop original image in pieces
+	 * @return array
+	 */
+	private function getImageInParts() {
+		$parts_tot = count($this->parts);
+		$tmp = array();
+		while(!is_null($key = key($this->parts))) {
+			$curr = current($this->parts);
+			$next = next($this->parts);
+			$tmp[] = substr($this->tmp_img, $curr, $next - $curr);
+			if ($next == $this->parts[$parts_tot - 1]) {
+				break;
+			}
+		}
+		return $tmp;
+	}
+	
+	private function getAdditions() {
+		// create some additional / repetitive image data
+		$additions = array();
+		for ($i = 1; $i < $this->chunks; $i++) {
+			$rs = array();
+			for($j = 0; $j < 2; $j++) {
+				$rs[] = rand($this->chunk_size_min, $this->chunk_size_max);
+			}
+			sort($rs);
+			$h = strlen($this->tmp_img) / rand(2,4);
+			$add = substr($this->tmp_img,$h - $rs[0], $h - $rs[1]);
+			$additions[] = $add;
+		}
+		return $additions;
 	}
 	
 	/**
@@ -102,14 +143,25 @@ class Satromizer {
 	 * @param int
 	 * @return array
 	 */
-	private function chunkValues($strlen) {
+	private function chunkValues() {
+		$strlen = strlen($this->tmp_img);
 		$parts = array();
 		for ($i = 1; $i < $this->chunks; $i++) {
 			$parts[] = rand(0,$strlen);
 		}
 		sort($parts);
-		array_unshift($parts,0);
-		array_push($parts,$strlen);
+		array_unshift($parts,0); // add 0 as minimum
+		array_push($parts,$strlen); // add strlen as max
 		return $parts;
+	}
+	
+	private function log($msg) {
+		@file_put_contents('imgs/log.txt',$msg,FILE_APPEND);
+	}
+	
+	public function __destruct() {
+		imagedestroy($this->orig_img);
+		imagedestroy($this->tmp_img);
+		imagedestroy($this->def_img);
 	}
 }
--- img.php
+++ img.php
@@ -2,12 +2,11 @@
 
 // Maleglitch
 require 'Satromizer.php';
-// error_reporting(E_ALL);
+error_reporting(E_ALL);
 
 // $s = new Satromizer('IMG_0007.JPG');
 // $s->show();
-header('Content-type: image/jpeg');
+// header('Content-type: image/jpeg');
 $s = new Satromizer('src/malevich.00'.rand(1,6).'.jpg');
-echo $s->show();
-$s->export('imgs/');
+echo $s->build()->export('imgs/')->show();
 exit;
