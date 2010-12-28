0e609abaa2d5586bb43113f0e0df9c7ef525aa39
Vincent Bruijn <vebruijn@gmail.com>
2010-12-28 20:09:25 +0100

    added extra images

--- index.php
+++ index.php
@@ -3,12 +3,35 @@
 
 <html version="HTML+RDFa 1.1" lang="en">
 <head>
+    <link rel="shortcut icon" href="favicon.ico">
 	<meta charset="utf-8">
 	<title>Kazimir Maleglitch :: ax710 :: y_a_v_a</title>
-	
-	<meta name="keywords" content="Maleglitch, Malevich, glitch, satromizer, ax710, y4v4, y_a_v_a, alweervincent">
+	<meta name="keywords" content="Maleglitch, Malevich, glitch, databending, satromizer, ax710, y4v4, y_a_v_a, alweervincent">
 	<meta name="description" content="Maleglitch by ax710 and y_a_v_a glitches Malevich paintings">
-
+    <meta name="project" content="
+      ___           ___                         ___           ___     
+     /__/\         /  /\                       /  /\         /  /\    
+    |  |::\       /  /::\                     /  /:/_       /  /:/_   
+    |  |:|:\     /  /:/\:\    ___     ___    /  /:/ /\     /  /:/ /\  
+  __|__|:|\:\   /  /:/~/::\  /__/\   /  /\  /  /:/ /:/_   /  /:/_/::\ 
+ /__/::::| \:\ /__/:/ /:/\:\ \  \:\ /  /:/ /__/:/ /:/ /\ /__/:/__\/\:\
+ \  \:\~~\__\/ \  \:\/:/__\/  \  \:\  /:/  \  \:\/:/ /:/ \  \:\ /~~/:/
+  \  \:\        \  \::/        \  \:\/:/    \  \::/ /:/   \  \:\  /:/ 
+   \  \:\        \  \:\         \  \::/      \  \:\/:/     \  \:\/:/  
+    \  \:\        \  \:\         \__\/        \  \::/       \  \::/   
+     \__\/         \__\/                       \__\/         \__\/    
+                                            ___           ___     
+                  ___           ___        /  /\         /__/\    
+                 /  /\         /  /\      /  /:/         \  \:\   
+  ___     ___   /  /:/        /  /:/     /  /:/           \__\:\  
+ /__/\   /  /\ /__/::\       /  /:/     /  /:/  ___   ___ /  /::\ 
+ \  \:\ /  /:/ \__\/\:\__   /  /::\    /__/:/  /  /\ /__/\  /:/\:\
+  \  \:\  /:/     \  \:\/\ /__/:/\:\   \  \:\ /  /:/ \  \:\/:/__\/
+   \  \:\/:/       \__\::/ \__\/  \:\   \  \:\  /:/   \  \::/     
+    \  \::/        /__/:/       \  \:\   \  \:\/:/     \  \:\     
+     \__\/         \__\/         \__\/    \  \::/       \  \:\    
+                                           \__\/         \__\/  
+                                        ">     
 <style type="text/css">
 html { width: 100%; height: 100%; background: url('img.php?q=<?php echo time(); ?>') 50% 50% no-repeat; background-size: auto 80%; -moz-background-size: auto 80%;}
 body { padding: 0; margin: 0; text-align: center; }
@@ -26,10 +49,9 @@ myReload = function() {
 </head>
 
 <body onload="setTimeout('myReload()', 10000)">
-
-	<h1>Kazimir Maleglitch</h1>
-	
-	<footer>
+		<h1><img src="maleglitch1003.gif" alt="" width="1037" height="300" border="0"></h1>
+		<footer>
+		<img src="maleglitchleft.gif" alt="" width="100" height="148" align="absmiddle" border="0">
 		<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">
 			<img alt="Creative Commons License" style="border-width:0; vertical-align: middle; clear: both; margin-right: 4px;" src="somerights20.jpg">
 		</a>
@@ -37,7 +59,7 @@ myReload = function() {
 		 van <a class="color1" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL">ax710</a> &amp; 
 		<a class="color2" xmlns:cc="http://creativecommons.org/ns#" href="http://www.alweervincent.nl/" property="cc:attributionName" rel="cc:attributionURL">y_a_v_a</a>
 		 is in licentie gegeven volgens een <a class="color3" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
-	</footer>
-
-</body>
+		<img src="maleglitchright.gif" alt="" width="100" height="148" align="absmiddle" border="0">
+		</footer>
+	</body>
 </html>
