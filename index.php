98d95b5f6ee8df94a2f94c1040e06759df12a88d
Vincent Bruijn <vebruijn@gmail.com>
2011-01-03 23:37:56 +0100

    added jquery, images and changed image location in html

--- index.php
+++ index.php
@@ -30,16 +30,18 @@
    \  \:\/:/       \__\::/ \__\/  \:\   \  \:\  /:/   \  \::/     
     \  \::/        /__/:/       \  \:\   \  \:\/:/     \  \:\     
      \__\/         \__\/         \__\/    \  \::/       \  \:\    
-                                           \__\/         \__\/  
-                                        ">     
+                                           \__\/         \__\/  ">
+<script type="text/javascript" src="jquery.js"></script>
 <style type="text/css">
-html { width: 100%; height: 100%; background: url('img.php?q=<?php echo time(); ?>') 50% 50% no-repeat; background-size: auto 80%; -moz-background-size: auto 80%;}
-body { padding: 0; margin: 0; text-align: center; }
+html { width: 100%; height: 100%; }
+body { padding: 0; margin: 0; text-align: center; overflow-x: hidden; }
 
 .color1 { color: rgb(236,   0, 140); }
 .color2 { color: rgb(  0, 174, 239); }
 .color3 { color: rgb(255, 242,   0); }
-footer { position: fixed; bottom: 0; width: 100%; text-align: center; display: block; margin: 10px 0;}
+#image { height: 0px; display: block; margin: 0 auto; }
+footer { width: 100%; text-align: center; display: block; margin: 10px 0; height: 71px; }
+.dsR1 { width: 1280px; height: 67px; }
 </style>
 <script type="text/javascript">
 myReload = function() {
@@ -49,17 +51,25 @@ myReload = function() {
 </head>
 
 <body onload="setTimeout('myReload()', 10000)">
-		<h1><img src="maleglitch1003.gif" alt="" width="1037" height="300" border="0"></h1>
-		<footer>
-		<img src="maleglitchleft.gif" alt="" width="100" height="148" align="absmiddle" border="0">
-		<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">
-			<img alt="Creative Commons License" style="border-width:0; vertical-align: middle; clear: both; margin-right: 4px;" src="somerights20.jpg">
-		</a>
-		<span class="color2" xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">Maleglitch</span>
-		 van <a class="color1" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL">ax710</a> &amp; 
-		<a class="color2" xmlns:cc="http://creativecommons.org/ns#" href="http://www.alweervincent.nl/" property="cc:attributionName" rel="cc:attributionURL">y_a_v_a</a>
-		 is in licentie gegeven volgens een <a class="color3" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
-		<img src="maleglitchright.gif" alt="" width="100" height="148" align="absmiddle" border="0">
+		<h1 id="header"><img class="dsR1" src="maleglitch1007.jpg" alt="Kazimir Maleglitch" border="0"></h1>
+		<img id="image" src="img.php?q=<?php echo time(); ?>" alt="Maleglitch.net">
+		<footer id="bottom">
+			<img src="maleglitchleft.jpg" alt="" width="48" height="71" align="middle" border="0">
+			<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">
+				<img alt="Creative Commons License" style="border-width:0; vertical-align: middle; clear: both; margin-right: 4px;" src="somerights20.jpg">
+			</a>
+			<span class="color2" xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">Maleglitch</span>
+			 van <a class="color1" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL">ax710</a> &amp; 
+			<a class="color2" xmlns:cc="http://creativecommons.org/ns#" href="http://www.alweervincent.nl/" property="cc:attributionName" rel="cc:attributionURL">y_a_v_a</a>
+			 is in licentie gegeven volgens een <a class="color3" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
+			<img src="maleglitchright.jpg" alt="" width="48" height="71" align="middle" border="0">
 		</footer>
+		<script type="text/javascript">
+			var h, f, d;
+			h = $('#header').outerHeight(1);
+			f = $('#bottom').outerHeight(1);
+			d = window.innerHeight - (h + f + 15);
+			$('#image').height(d);
+		</script>
 	</body>
 </html>
