deb4109144e0151cb7467dda5fda7c39b8265e6a
Vincent Bruijn <vebruijn@gmail.com>
2014-02-21 21:05:03 +0100

    added new header, added some meta tags, added favicon

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@
     <link rel="shortcut icon" href="favicon.ico">
 	<meta charset="utf-8">
 	<title>Kazimir Maleglitch :: ax710 :: y_a_v_a</title>
-	<meta name="keywords" content="Maleglitch, Malevich, glitch, databending, satromizer, ax710, y4v4, y_a_v_a, alweervincent, suprematism, suprematist composition, hypersuprematism, hyper-suprematism, hypersuprematist composition, hyper-suprematist composition">
+	<meta name="keywords" content="Maleglitch, Kazimir Malevich, glitch, glitch art, databending, satromizer, ax710, y4v4, y_a_v_a, alweervincent, suprematism, suprematist composition, hypersuprematism, hyper-suprematism, hypersuprematist composition, hyper-suprematist composition">
 	<meta name="description" content="Maleglitch by ax710 and y_a_v_a glitches Malevich paintings">
     <meta name="project" content="
       ___           ___                         ___           ___     
@@ -35,13 +35,13 @@
 <style type="text/css">
 html { width: 100%; height: 100%; }
 body { padding: 0; margin: 0; text-align: center; overflow-x: hidden; }
-
+img { border: none; }
 .color1 { color: rgb(236,   0, 140); }
 .color2 { color: rgb(  0, 174, 239); }
 .color3 { color: rgb(255, 242,   0); }
 #image { height: 0px; display: block; margin: 0 auto; }
 footer { width: 100%; text-align: center; display: block; margin: 10px 0; height: 71px; }
-.dsR1 { width: 1280px; height: 67px; }
+.dsR1 { width: 700px; height: 100px; }
 </style>
 <script type="text/javascript">
 myReload = function() {
@@ -51,12 +51,11 @@ myReload = function() {
 </head>
 
 <body onload="setTimeout('myReload()', 20000)">
-		<h1 id="header"><img class="dsR1" src="maleglitch1007.jpg" alt="Kazimir Maleglitch" border="0"></h1>
+		<h1 id="header" title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><img class="dsR1" src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></h1>
 		<img id="image" src="img.php?q=<?php echo time(); ?>" alt="Maleglitch.net">
 		<h2>Hypersuprematist composition</h2>
 		<a href="rss.php" title="RSS feed to get your regular Hypersuprematist composition">Maleglitch rss feed</a> - <a href="api.php" title="JSON API for getting random Hypersuprematist compositions.">Maleglitch API</a>
 		<footer id="bottom">
-			<img src="maleglitchleft.jpg" alt="" width="48" height="71" align="middle" border="0">
 			<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">
 				<img alt="Creative Commons License" style="border-width:0; vertical-align: middle; clear: both; margin-right: 4px;" src="somerights20.jpg">
 			</a>
@@ -64,13 +63,12 @@ myReload = function() {
 			 van <a class="color1" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL" title="http://ax710.org/">ax710</a> &amp; 
 			<a class="color2" xmlns:cc="http://creativecommons.org/ns#" href="http://www.y-a-v-a.org/" property="cc:attributionName" rel="cc:attributionURL" title="yet another visual artist">y_a_v_a</a>
 			 is in licentie gegeven volgens een <a class="color3" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
-			<img src="maleglitchright.jpg" alt="" width="48" height="71" align="middle" border="0">
 		</footer>
 		<script type="text/javascript">
 			var h, f, d;
 			h = $('#header').outerHeight(1);
 			f = $('#bottom').outerHeight(1);
-			console.log(h,f);
+
 			d = window.innerHeight - (h + f + 80);
 			$('#image').height(d);
 		</script>
