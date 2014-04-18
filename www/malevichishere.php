af8fd21d5fda424cb4074483c95894d9d8a5deda
Vincent Bruijn <vebruijn@gmail.com>
2014-04-18 09:22:55 +0200

    added text about winning tattoo

---
+++ www/malevichishere.php
@@ -0,0 +1,56 @@
+<?php header("Content-Type: text/html; charset=UTF-8"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="shortcut icon" href="favicon.ico">
+	<meta charset="utf-8">
+    <title>Maleglitch :: #malevichishere :: ax710 :: y_a_v_a</title>
+   	<meta name="keywords" content="Maleglitch, Kazimir Malevich, malevichishere, Malevich tattoo, Henk Schiffmacher, Stedelijk Museum Amsterdam">
+   	<meta name="description" content="Maleglitch by ax710 and y_a_v_a glitches Malevich paintings">
+       <meta name="project" content="
+         ___           ___                         ___           ___     
+        /__/\         /  /\                       /  /\         /  /\    
+       |  |::\       /  /::\                     /  /:/_       /  /:/_   
+       |  |:|:\     /  /:/\:\    ___     ___    /  /:/ /\     /  /:/ /\  
+     __|__|:|\:\   /  /:/~/::\  /__/\   /  /\  /  /:/ /:/_   /  /:/_/::\ 
+    /__/::::| \:\ /__/:/ /:/\:\ \  \:\ /  /:/ /__/:/ /:/ /\ /__/:/__\/\:\
+    \  \:\~~\__\/ \  \:\/:/__\/  \  \:\  /:/  \  \:\/:/ /:/ \  \:\ /~~/:/
+     \  \:\        \  \::/        \  \:\/:/    \  \::/ /:/   \  \:\  /:/ 
+      \  \:\        \  \:\         \  \::/      \  \:\/:/     \  \:\/:/  
+       \  \:\        \  \:\         \__\/        \  \::/       \  \::/   
+        \__\/         \__\/                       \__\/         \__\/    
+                                               ___           ___     
+                     ___           ___        /  /\         /__/\    
+                    /  /\         /  /\      /  /:/         \  \:\   
+     ___     ___   /  /:/        /  /:/     /  /:/           \__\:\  
+    /__/\   /  /\ /__/::\       /  /:/     /  /:/  ___   ___ /  /::\ 
+    \  \:\ /  /:/ \__\/\:\__   /  /::\    /__/:/  /  /\ /__/\  /:/\:\
+     \  \:\  /:/     \  \:\/\ /__/:/\:\   \  \:\ /  /:/ \  \:\/:/__\/
+      \  \:\/:/       \__\::/ \__\/  \:\   \  \:\  /:/   \  \::/     
+       \  \::/        /__/:/       \  \:\   \  \:\/:/     \  \:\     
+        \__\/         \__\/         \__\/    \  \::/       \  \:\    
+                                              \__\/         \__\/  ">
+<style type="text/css">
+html { width: 100%; height: 100%; }
+body { padding: 0; margin: 0; text-align: center; overflow-x: hidden; }
+img { border: none; }
+.color1 { color: rgb(236,   0, 140); }
+.color2 { color: rgb(  0, 174, 239); }
+.color3 { color: rgb(255, 242,   0); }
+.content { width: 700px; text-align: left; margin: 0 auto 240px auto;}
+.content p { line-height: 1.8; font-size: 20px;}
+</style>
+</head>
+<body>
+    <h1 id="header" title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><a href="/"><img class="dsR1" src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></a></h1>
+
+        <div class="content">
+            <h1>#malevichishere</h1>
+            <p>ax710 and y_a_v_a won the <a class="color1" href="http://www.stedelijk.nl/malevichishere">#malevichishere contest</a> of the Stedelijk Museum Amsterdam in February 2014. This contest was held at the event of the <strong>Kazimir Malevich and the Russian Avant-garde</strong> exhibition. The idea was to send in a creative work, inspired by Kazimir Malevich's art. We sent in this site: <a class="color2" href="/">Maleglitch.net</a>.</p>
+            <img src="tattoo/Malevich_Tattoo_Hanky_Panky_Ruud_Akse-08a.jpg" alt="" width="700">
+            <p>The price was a Malevich tattoo, to be tattooed by Dutch tattoo legend Henk Schiffmacher. The idea came from Bart Rutten, curator at the Stedelijk Museum Amsterdam, who once met a girl with a Malevich tattoo. <strong>We won the contest so we now have a Malevich tattoo!</strong></p>
+            <img src="tattoo/Malevich_Tattoo_Hanky_Panky_Ruud_Akse-04a.jpg" alt="" width="700">
+            <p>Or more precisely: Since he was the one that submitted the work, y_a_v_a won the competition and the tattoo, according to the Stedelijk Museum. But because we created Maleglitch together, we decided that both of us are winners, so ax710 also had himself tattooed. His is a <em>Maleglitch</em> tattoo of the painting that y_a_v_a's tattoo is based on. This makes the circle round: y_a_v_a has a <strong>Malevich</strong> tattoo, ax710 has a <u>Maleglitch</u> tattoo.</p><p>The pictures shown here are taken by Zwaan Ipema at <a class="color1" href="http://www.tattooing.nl/">Schiffmacher &amp; Veldhoen</a> on March 15 2014. More beautiful pictures taken by <a class="color2" href="http://www.tomekdersuaaron.com/home/">Tomek Whitfield</a> can be found on the <a class="color1" href="https://www.facebook.com/media/set/?set=a.10152266729704659.1073741849.103829284658">Stedelijk Museum Facebook page</a>. Other submissions to the contest can be viewed on <a class="color3" href="http://www.pinterest.com/stedelijk/malevichishere/">Pinterest</a>.</p>
+        </div>
+</body>
+</html>
