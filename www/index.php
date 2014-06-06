9219c92245d73a40aacb658051d290f370860423
Vincent Bruijn <vebruijn@gmail.com>
2014-06-06 17:37:26 +0200

    removed jquery, now everything vanilla js, removed most css classes, added sourcecode text

--- www/index.php
+++ www/index.php
@@ -8,7 +8,9 @@
 	<title>Kazimir Maleglitch :: ax710 :: y_a_v_a</title>
 	<meta name="keywords" content="Maleglitch, Kazimir Malevich, glitch, glitch art, databending, satromizer, ax710, y4v4, y_a_v_a, alweervincent, suprematism, suprematist composition, hypersuprematism, hyper-suprematism, hypersuprematist composition, hyper-suprematist composition">
 	<meta name="description" content="Maleglitch by ax710 and y_a_v_a glitches Malevich paintings">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="project" content="
+    
       ___           ___                         ___           ___     
      /__/\         /  /\                       /  /\         /  /\    
     |  |::\       /  /::\                     /  /:/_       /  /:/_   
@@ -31,57 +33,165 @@
     \  \::/        /__/:/       \  \:\   \  \:\/:/     \  \:\     
      \__\/         \__\/         \__\/    \  \::/       \  \:\    
                                            \__\/         \__\/  ">
-<script type="text/javascript" src="jquery.js"></script>
-<style type="text/css">
+<style>
 html { width: 100%; height: 100%; }
-body { padding: 0; margin: 0; text-align: center; overflow-x: hidden; }
+body { height: 100%; padding: 0; margin: 0; text-align: center; overflow-x: hidden; }
 img { border: none; }
-.color1 { color: rgb(236,   0, 140); }
-.color2 { color: rgb(  0, 174, 239); }
-.color3 { color: rgb(255, 242,   0); }
-#image { height: 0px; display: block; margin: 0 auto; }
-footer { width: 100%; text-align: center; display: block; margin: 10px 0; height: 71px; }
-.dsR1 { width: 700px; height: 100px; }
-</style>
-<script type="text/javascript">
-myReload = function() {
-	window.location.reload();
+.c { color: rgb(  0, 174, 239); }
+.m { color: rgb(236,   0, 140); }
+.y { color: rgb(255, 242,   0); }
+h1 + img { display: block; height: 0; margin: 0 auto; }
+footer { display: block; width: 100%; height: 71px; margin: 10px 0; text-align: center; }
+h1 > img { width: 700px; height: 100px; }
+footer + div { position: fixed; width: 100%; height: 3px; bottom: 0; left: 0; line-height: 0; }
+footer + div > div {  float: left; width: 0; height: 3px; line-height: 0; background-color: rgba(236,   0, 140, .3); }
+img[alt="Creative Commons License"] { margin-right: 4px; clear: both; vertical-align: middle; }
+@media screen and (max-width: 1024px) {
+    h2 { margin: 0.4em 0; }
+    footer { height: 40px; margin-bottom: 0; }
 }
-</script>
+@media screen and (max-width: 400px) {
+    body { font-size: 11px;}
+    h1 { margin: 4px 0 8px 0; line-height: 1; }
+    h1 > img { width: 100%; height: auto; }
+    img { max-width: 100%; }
+    h2 { margin: 0.4em 0; font-size: 18px; }
+    img[alt="Creative Commons License"] { width: 45px; }
+}
+</style>
 </head>
 
-<body onload="setTimeout('myReload()', 20000)">
-		<h1 id="header" title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><img class="dsR1" src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></h1>
-		<img id="image" src="img.php?q=<?php echo time(); ?>" alt="Maleglitch.net">
+<body>
+		<h1 title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><img src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></h1>
+		<img src="img.php?q=<?php echo time() . '000'; ?>" alt="Maleglitch.net">
 		<h2>Hypersuprematist composition</h2>
-		<a href="rss.php" title="RSS feed to get your regular Hypersuprematist composition">Maleglitch rss feed</a> - <a href="api.php" title="JSON API for getting random Hypersuprematist compositions.">Maleglitch API</a>
-		<footer id="bottom">
-			<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">
-				<img alt="Creative Commons License" style="border-width:0; vertical-align: middle; clear: both; margin-right: 4px;" src="somerights20.jpg">
-			</a>
-			<span class="color2" xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">Maleglitch</span>
-			 van <a class="color1" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL" title="http://ax710.org/">ax710</a> &amp; 
-			<a class="color2" xmlns:cc="http://creativecommons.org/ns#" href="http://www.y-a-v-a.org/" property="cc:attributionName" rel="cc:attributionURL" title="yet another visual artist">y_a_v_a</a>
-			 is in licentie gegeven volgens een <a class="color3" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
+		<a class="c" href="rss.php" title="RSS feed to get your regular Hypersuprematist composition">Maleglitch rss feed</a> -
+        <a class="y" href="api.php" title="JSON API for getting random Hypersuprematist compositions.">Maleglitch API</a> -
+        <a class="m" href="malevichishere.php">#malevichishere tattoo</a>
+		<footer>
+			<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/"><img alt="Creative Commons License" src="somerights20.jpg"></a>
+			<span class="c" xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">Maleglitch</span>
+			 van <a class="m" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL" title="http://ax710.org/">ax710</a> &amp; 
+			<a class="c" xmlns:cc="http://creativecommons.org/ns#" href="http://www.y-a-v-a.org/" property="cc:attributionName" rel="cc:attributionURL" title="yet another visual artist">y_a_v_a</a>
+			 is in licentie gegeven volgens een <a class="y" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
 		</footer>
-		<script type="text/javascript">
-			var h, f, d;
-			h = $('#header').outerHeight(1);
-			f = $('#bottom').outerHeight(1);
+        <div><div></div></div>
+		<script>
+            /**
+             * Dear source code viewer
+             * 
+             * We consider this website a piece of art. Whether you agree on this or not,
+             * is fine with us. But you'd probably agree on that making art requires
+             * focus, concentration and the will to reach perfection.
+             *
+             * Next to making this site visually as perfect as we wish, we also strive
+             * to perfectionate the usage of the materials it is built with.
+             * We try to create code that complies with our standards. This means we want
+             * to use the 3 core languages of the Web, HTML, CSS and JavaScript, as pure
+             * as possible. That's why we do not use any libraries for the layout of our
+             * site and try to minimize the usage of CSS classes or ids, just to see how
+             * far we can get. Enjoy!
+             */
+            (function() {
+                'use strict';
+                // polyfill for requestAnimationFrame
+                if (!Date.now) {
+                    Date.now = function() {
+                        return (new Date()).getTime();
+                    };
+                }
 
-			d = window.innerHeight - (h + f + 80);
-			$('#image').height(d);
+                (function() {
+                    var vendors = ['webkit', 'moz']
+                    , i = 0
+                    , prefix,
+                    lastTime;
+                    for (; i < vendors.length && !window.requestAnimationFrame; ++i) {
+                        prefix = vendors[i];
+                        window.requestAnimationFrame = window[prefix + 'RequestAnimationFrame'];
+                        window.cancelAnimationFrame = (window[prefix + 'CancelAnimationFrame'] || window[prefix + 'CancelRequestAnimationFrame']);
+                    }
+                    if (/iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent) || // iOS6 is buggy
+                        !window.requestAnimationFrame || !window.cancelAnimationFrame) {
+                        lastTime = 0;
+                        window.requestAnimationFrame = function(callback) {
+                            var now = Date.now(),
+                            nextTime = Math.max(lastTime + 16, now);
+                            return setTimeout(function() { callback(lastTime = nextTime); }, nextTime - now);
+                        };
+                        window.cancelAnimationFrame = clearTimeout;
+                    }
+                }());
+
+                /**
+                 * Calculate the maximum possible height of the image
+                 */
+                (function() {
+                    var header = document.getElementsByTagName('h1')[0],
+                    footer = document.getElementsByTagName('footer')[0],
+        			h, f, d;
+
+                    h = parseFloat(window.getComputedStyle(header).height);
+                    h += parseFloat(window.getComputedStyle(header).marginTop);
+                    h += parseFloat(window.getComputedStyle(header).marginBottom);
+
+                    f = parseFloat(window.getComputedStyle(footer).height);
+                    f += parseFloat(window.getComputedStyle(footer).marginTop);
+                    f += parseFloat(window.getComputedStyle(footer).marginBottom);
+
+        			d = window.innerHeight - (h + f + 80);
+        			document.getElementsByTagName('img')[1].style.height = d + 'px';
+                }());
+            
+                /**
+                 * Function to change image source
+                 */
+                var reloadMaleglitch = function reloadMaleglitch() {
+                    var el = document.getElementsByTagName('img')[1]
+                    , path = el.src;
+                    path = path.replace(/\?q=.*$/, '?q=' + (new Date()).getTime());
+                    el.src = path;
+                }
+                , handle
+                , interval
+                , duration = 8
+                , width = 0
+                , past = 0
+                , step = (document.documentElement.clientWidth / duration)
+                , slide = document.getElementsByTagName('div')[1]
+                /**
+                 * Function to animate width of timer
+                 */
+                , draw = function draw(time) {
+                    var delta = (time || 0) - past
+                    , jump = (delta === 0) ? 0 : (step * (delta / 1000));
+                    handle = window.requestAnimationFrame(draw);
+                
+                    slide.style.width = (width += jump) + 'px';
+                    past = time || 0;
+                },
+                timer = function timer() {
+                    window.cancelAnimationFrame(handle);
+                    width = 0;
+                    reloadMaleglitch();
+                    draw();
+                };
+
+                timer();
+                interval = setInterval(timer, duration * 1000);
+            }());
+
+            // Google analytics
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-16179947-10']);
+            _gaq.push(['_setDomainName', 'maleglitch.net']);
+            _gaq.push(['_trackPageview']);
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = 'http://www.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
 		</script>
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-16179947-10']);
-		  _gaq.push(['_setDomainName', 'maleglitch.net']);
-		  _gaq.push(['_trackPageview']);
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = 'http://www.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>
+        
 	</body>
 </html>
--- www/malevichishere.php
+++ www/malevichishere.php
@@ -7,6 +7,7 @@
     <title>Maleglitch :: #malevichishere :: ax710 :: y_a_v_a</title>
    	<meta name="keywords" content="Maleglitch, Kazimir Malevich, malevichishere, Malevich tattoo, Henk Schiffmacher, Stedelijk Museum Amsterdam">
    	<meta name="description" content="Maleglitch by ax710 and y_a_v_a glitches Malevich paintings">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
        <meta name="project" content="
          ___           ___                         ___           ___     
         /__/\         /  /\                       /  /\         /  /\    
@@ -30,27 +31,42 @@
        \  \::/        /__/:/       \  \:\   \  \:\/:/     \  \:\     
         \__\/         \__\/         \__\/    \  \::/       \  \:\    
                                               \__\/         \__\/  ">
-<style type="text/css">
+<style>
 html { width: 100%; height: 100%; }
 body { padding: 0; margin: 0; text-align: center; overflow-x: hidden; }
 img { border: none; }
-.color1 { color: rgb(236,   0, 140); }
-.color2 { color: rgb(  0, 174, 239); }
-.color3 { color: rgb(255, 242,   0); }
-.content { width: 700px; text-align: left; margin: 0 auto 240px auto;}
-.content p { line-height: 1.8; font-size: 20px;}
+.c { color: rgb(  0, 174, 239); }
+.m { color: rgb(236,   0, 140); }
+.y { color: rgb(255, 242,   0); }
+body > div { width: 700px; margin: 0 auto 240px auto; text-align: left; }
+body > div p { line-height: 1.8; font-size: 20px; }
+@media screen and (max-width: 400px) {
+    h1 { margin: 4px 0 8px 0; line-height: 1; }
+    h1 a > img { width: 100%; height: auto; }
+    h2 { margin: 0.4em 0; font-size: 22px; }
+    body > div {
+        width: 100%;
+    }
+    body > div img {
+        width: 100%;
+    }
+    body > div p {
+        padding: 0 10px;
+    }
+}
 </style>
 </head>
 <body>
-    <h1 id="header" title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><a href="/"><img class="dsR1" src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></a></h1>
+    <h1 id="header" title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><a href="/"><img src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></a></h1>
 
-        <div class="content">
+        <div>
             <h1>#malevichishere</h1>
-            <p>ax710 and y_a_v_a won the <a class="color1" href="http://www.stedelijk.nl/malevichishere">#malevichishere contest</a> of the Stedelijk Museum Amsterdam in February 2014. This contest was held at the event of the <strong>Kazimir Malevich and the Russian Avant-garde</strong> exhibition. The idea was to send in a creative work, inspired by Kazimir Malevich's art. We sent in this site: <a class="color2" href="/">Maleglitch.net</a>.</p>
+            <p>ax710 and y_a_v_a won the <a class="m" href="http://www.stedelijk.nl/malevichishere">#malevichishere contest</a> of the Stedelijk Museum Amsterdam in February 2014. This contest was held at the event of the <strong>Kazimir Malevich and the Russian Avant-garde</strong> exhibition. The idea was to send in a creative work, inspired by Kazimir Malevich's art. We sent in this site: <a class="c" href="/">Maleglitch.net</a>.</p>
             <img src="tattoo/Malevich_Tattoo_Hanky_Panky_Ruud_Akse-08a.jpg" alt="" width="700">
             <p>The price was a Malevich tattoo, to be tattooed by Dutch tattoo legend Henk Schiffmacher. The idea came from Bart Rutten, curator at the Stedelijk Museum Amsterdam, who once met a girl with a Malevich tattoo. <strong>We won the contest so we now have a Malevich tattoo!</strong></p>
             <img src="tattoo/Malevich_Tattoo_Hanky_Panky_Ruud_Akse-04a.jpg" alt="" width="700">
-            <p>Or more precisely: Since he was the one that submitted the work, y_a_v_a won the competition and the tattoo, according to the Stedelijk Museum. But because we created Maleglitch together, we decided that both of us are winners, so ax710 also had himself tattooed. His is a <em>Maleglitch</em> tattoo of the painting that y_a_v_a's tattoo is based on. This makes the circle round: y_a_v_a has a <strong>Malevich</strong> tattoo, ax710 has a <u>Maleglitch</u> tattoo.</p><p>The pictures shown here are taken by Zwaan Ipema at <a class="color1" href="http://www.tattooing.nl/">Schiffmacher &amp; Veldhoen</a> on March 15 2014. More beautiful pictures taken by <a class="color2" href="http://www.tomekdersuaaron.com/home/">Tomek Whitfield</a> can be found on the <a class="color1" href="https://www.facebook.com/media/set/?set=a.10152266729704659.1073741849.103829284658">Stedelijk Museum Facebook page</a>. Other submissions to the contest can be viewed on <a class="color3" href="http://www.pinterest.com/stedelijk/malevichishere/">Pinterest</a>.</p>
+            <p>Or more precisely: <a class="c" href="http://www.ax710.org" target="_blank">ax710</a> has a Maleglitch tattoo of the painting that <a class="y" href="http://www.y-a-v-a.org" target="_blank">y_a_v_a</a>'s tattoo is based on. This makes the circle round: y_a_v_a has a Malevich tattoo, ax710 has a Maleglitch tattoo.</p><p>The pictures shown here are taken by Zwaan Ipema at <a class="m" href="http://www.tattooing.nl/">Schiffmacher &amp; Veldhoen</a> on March 15 2014. More beautiful pictures taken by <a class="c" href="http://www.tomekdersuaaron.com/home/">Tomek Whitfield</a> can be found on the <a class="m" href="https://www.facebook.com/media/set/?set=a.10152266729704659.1073741849.103829284658">Stedelijk Museum Facebook page</a>. Other submissions to the contest can be viewed on <a class="y" href="http://www.pinterest.com/stedelijk/malevichishere/">Pinterest</a>.</p>
+            <p><a class="c" href="http://www.maleglitch.net/">&larr;&nbsp;back</a></p>
         </div>
 </body>
 </html>
