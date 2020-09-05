76d73666d3b70cdeae0ed4a24869362cb2ce31c8
Vincent Bruijn <vebruijn@gmail.com>
2020-09-05 20:34:11 +0200

    updated css and js to more modern implementation

--- www/index.php
+++ www/index.php
@@ -1,197 +1,147 @@
 <?php header("Content-Type: text/html; charset=UTF-8"); ?>
 <!DOCTYPE html>
-
 <html version="HTML+RDFa 1.1" lang="en">
 <head>
-    <link rel="shortcut icon" href="favicon.ico">
+  <link rel="shortcut icon" href="favicon.ico">
 	<meta charset="utf-8">
 	<title>Kazimir Maleglitch :: ax710 :: y_a_v_a</title>
 	<meta name="keywords" content="Maleglitch, Kazimir Malevich, glitch, glitch art, databending, satromizer, ax710, y4v4, y_a_v_a, alweervincent, suprematism, suprematist composition, hypersuprematism, hyper-suprematism, hypersuprematist composition, hyper-suprematist composition">
 	<meta name="description" content="Maleglitch by ax710 and y_a_v_a glitches Malevich paintings">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="project" content="
-    
-      ___           ___                         ___           ___     
-     /__/\         /  /\                       /  /\         /  /\    
-    |  |::\       /  /::\                     /  /:/_       /  /:/_   
-    |  |:|:\     /  /:/\:\    ___     ___    /  /:/ /\     /  /:/ /\  
-  __|__|:|\:\   /  /:/~/::\  /__/\   /  /\  /  /:/ /:/_   /  /:/_/::\ 
+
+      ___           ___                         ___           ___
+     /__/\         /  /\                       /  /\         /  /\
+    |  |::\       /  /::\                     /  /:/_       /  /:/_
+    |  |:|:\     /  /:/\:\    ___     ___    /  /:/ /\     /  /:/ /\
+  __|__|:|\:\   /  /:/~/::\  /__/\   /  /\  /  /:/ /:/_   /  /:/_/::\
  /__/::::| \:\ /__/:/ /:/\:\ \  \:\ /  /:/ /__/:/ /:/ /\ /__/:/__\/\:\
  \  \:\~~\__\/ \  \:\/:/__\/  \  \:\  /:/  \  \:\/:/ /:/ \  \:\ /~~/:/
-  \  \:\        \  \::/        \  \:\/:/    \  \::/ /:/   \  \:\  /:/ 
-   \  \:\        \  \:\         \  \::/      \  \:\/:/     \  \:\/:/  
-    \  \:\        \  \:\         \__\/        \  \::/       \  \::/   
-     \__\/         \__\/                       \__\/         \__\/    
-                                            ___           ___     
-                  ___           ___        /  /\         /__/\    
-                 /  /\         /  /\      /  /:/         \  \:\   
-  ___     ___   /  /:/        /  /:/     /  /:/           \__\:\  
- /__/\   /  /\ /__/::\       /  /:/     /  /:/  ___   ___ /  /::\ 
+  \  \:\        \  \::/        \  \:\/:/    \  \::/ /:/   \  \:\  /:/
+   \  \:\        \  \:\         \  \::/      \  \:\/:/     \  \:\/:/
+    \  \:\        \  \:\         \__\/        \  \::/       \  \::/
+     \__\/         \__\/                       \__\/         \__\/
+                                            ___           ___
+                  ___           ___        /  /\         /__/\
+                 /  /\         /  /\      /  /:/         \  \:\
+  ___     ___   /  /:/        /  /:/     /  /:/           \__\:\
+ /__/\   /  /\ /__/::\       /  /:/     /  /:/  ___   ___ /  /::\
  \  \:\ /  /:/ \__\/\:\__   /  /::\    /__/:/  /  /\ /__/\  /:/\:\
   \  \:\  /:/     \  \:\/\ /__/:/\:\   \  \:\ /  /:/ \  \:\/:/__\/
-   \  \:\/:/       \__\::/ \__\/  \:\   \  \:\  /:/   \  \::/     
-    \  \::/        /__/:/       \  \:\   \  \:\/:/     \  \:\     
-     \__\/         \__\/         \__\/    \  \::/       \  \:\    
+   \  \:\/:/       \__\::/ \__\/  \:\   \  \:\  /:/   \  \::/
+    \  \::/        /__/:/       \  \:\   \  \:\/:/     \  \:\
+     \__\/         \__\/         \__\/    \  \::/       \  \:\
                                            \__\/         \__\/  ">
 <style>
-html { width: 100%; height: 100%; }
-body { height: 100%; padding: 0; margin: 0; text-align: center; overflow-x: hidden; }
+html { width: 100vw; height: 100vh; font-size: 16px; }
+body { height: 100%; padding: 0; margin: 0; text-align: center; /*overflow: hidden;*/ }
+article {display: flex; flex-direction: column; height: 100%;}
 img { border: none; }
 .c { color: rgb(  0, 174, 239); }
 .m { color: rgb(236,   0, 140); }
 .y { color: rgb(255, 242,   0); }
-h1 + img { display: block; height: 0; margin: 0 auto; }
-footer { display: block; width: 100%; height: 71px; margin: 10px 0; text-align: center; }
-h1 > img { width: 700px; height: 100px; }
-footer + div { position: fixed; width: 100%; height: 3px; bottom: 0; left: 0; line-height: 0; }
-footer + div > div {  float: left; width: 0; height: 3px; line-height: 0; background-color: rgba(236,   0, 140, .3); }
+
+/* h1 + img { display: block; margin: 0 auto; max-width: 100%;} */
+figure { margin: 0; flex-grow: 2; position: relative; }
+figure img { max-width: 100%; position: absolute; top: 0; right: 0; bottom: 0; left: 0;margin: auto; max-height: 100%;  }
+footer { margin: 1em 0; }
+h1 > img { max-width: 100%; }
+article + div { position: fixed; width: 100%; height: 0.1875em; bottom: 0; left: 0; line-height: 0; }
+article + div > div {  float: left; width: 100%; height: 0.1875em; line-height: 0; background-color: rgba(236,   0, 140, .3); }
 img[alt="Creative Commons License"] { margin-right: 4px; clear: both; vertical-align: middle; }
-@media screen and (max-width: 1024px) {
-    h2 { margin: 0.4em 0; }
-    footer { height: 40px; margin-bottom: 0; }
-}
-@media screen and (max-width: 400px) {
-    body { font-size: 11px;}
-    h1 { margin: 4px 0 8px 0; line-height: 1; }
-    h1 > img { width: 100%; height: auto; }
-    img { max-width: 100%; }
-    h2 { margin: 0.4em 0; font-size: 18px; }
-    img[alt="Creative Commons License"] { width: 45px; }
+
+@media screen and (max-width: 414px) {
+  body { font-size: 18px;}
 }
 </style>
 </head>
-
 <body>
-		<h1 title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><img src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></h1>
-		<img src="img.php?q=<?php echo time() . '000'; ?>" alt="Maleglitch.net">
-		<h2>Hypersuprematist composition</h2>
-		<a class="c" href="rss.php" title="RSS feed to get your regular Hypersuprematist composition">Maleglitch rss feed</a> -
-        <a class="y" href="api.php" title="JSON API for getting random Hypersuprematist compositions.">Maleglitch API</a> -
-        <a class="m" href="malevichishere.php">#malevichishere tattoo</a>
-		<footer>
-			<a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/"><img alt="Creative Commons License" src="somerights20.jpg"></a>
-			<span class="c" xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">Maleglitch</span>
-			 van <a class="m" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL" title="http://ax710.org/">ax710</a> &amp; 
-			<a class="c" xmlns:cc="http://creativecommons.org/ns#" href="http://www.y-a-v-a.org/" property="cc:attributionName" rel="cc:attributionURL" title="yet another visual artist">y_a_v_a</a>
-			 is in licentie gegeven volgens een <a class="y" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
-		</footer>
-        <div><div></div></div>
-		<script>
-            /**
-             * Dear source code viewer
-             * 
-             * We consider this website a piece of art. Whether you agree on this or not,
-             * is fine with us. But you'd probably agree on that making art requires
-             * focus, concentration and the will to reach perfection.
-             *
-             * Next to making this site visually as perfect as we wish, we also strive
-             * to perfectionate the usage of the materials it is built with.
-             * We try to create code that complies with our standards. This means we want
-             * to use the 3 core languages of the Web, HTML, CSS and JavaScript, as pure
-             * as possible. That's why we do not use any libraries for the layout of our
-             * site and try to minimize the usage of CSS classes or ids, just to see how
-             * far we can get. Enjoy!
-             */
-            (function() {
-                'use strict';
-                // polyfill for requestAnimationFrame
-                if (!Date.now) {
-                    Date.now = function() {
-                        return (new Date()).getTime();
-                    };
-                }
+  <article class="">
+    <h1 title="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"><img src="header.jpg" alt="Kazimir Maleglitch by ax710 and y-a-v-a - 2010"></h1>
+    <figure>
+      <img src="img.php?q=<?php echo time() . '000'; ?>" alt="Maleglitch.net">
+    </figure>
+    <div>
+      <h2>Hypersuprematist composition</h2>
+      <a class="c" href="rss.php" title="RSS feed to get your regular Hypersuprematist composition">Maleglitch rss feed</a> -
+      <a class="y" href="api.php" title="JSON API for getting random Hypersuprematist compositions.">Maleglitch API</a> -
+      <a class="m" href="malevichishere.php">#malevichishere tattoo</a>
+    </div>
+    <footer>
+      <a rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/"><img alt="Creative Commons License" src="somerights20.jpg"></a>
+      <span class="c" xmlns:dc="http://purl.org/dc/elements/1.1/" property="dc:title">Maleglitch</span>
+        van <a class="m" xmlns:cc="http://creativecommons.org/ns" href="http://www.ax710.org/" property="cc:attributionName" rel="cc:attributionURL" title="http://ax710.org/">ax710</a> &amp;
+      <a class="c" xmlns:cc="http://creativecommons.org/ns#" href="http://www.y-a-v-a.org/" property="cc:attributionName" rel="cc:attributionURL" title="yet another visual artist">y_a_v_a</a>
+        is in licentie gegeven volgens een <a class="y" rel="license" href="http://creativecommons.org/licenses/by/3.0/nl/">Creative Commons Naamsvermelding 3.0 Nederland licentie</a>.
+    </footer>
+  </article>
+  <div><div></div></div>
+  <script>
+    /**
+      * Dear source code viewer
+      *
+      * We consider this website a piece of art. Whether you agree on this or not,
+      * is fine with us. But you'd probably agree on that making art requires
+      * focus, concentration and the will to reach perfection.
+      *
+      * Next to making this site visually as perfect as we wish, we also strive
+      * to perfectionate the usage of the materials it is built with.
+      * We try to create code that complies with our standards. This means we want
+      * to use the 3 core languages of the Web, HTML, CSS and JavaScript, as pure
+      * as possible. That's why we do not use any libraries for the layout of our
+      * site and try to minimize the usage of CSS classes or ids, just to see how
+      * far we can get. Enjoy!
+      */
+    (function() {
+      /**
+        * Function to change image source
+        */
+      var reloadMaleglitch = function reloadMaleglitch() {
+        var el = document.getElementsByTagName('img')[1]
+        , path = el.src;
+        path = path.replace(/\?q=.*$/, '?q=' + (new Date()).getTime());
+        el.src = path;
+      }
+      , handle
+      , interval
+      , duration = 8
+      , width = 0
+      , past = 0
+      , step = (document.documentElement.clientWidth / duration)
+      , slide = document.getElementsByTagName('div')[1]
+      /**
+        * Function to animate width of timer
+        */
+      , draw = function draw(time) {
+        var delta = (time || 0) - past
+        , jump = (delta === 0) ? 0 : (step * (delta / 1000));
+        handle = window.requestAnimationFrame(draw);
 
-                (function() {
-                    var vendors = ['webkit', 'moz']
-                    , i = 0
-                    , prefix,
-                    lastTime;
-                    for (; i < vendors.length && !window.requestAnimationFrame; ++i) {
-                        prefix = vendors[i];
-                        window.requestAnimationFrame = window[prefix + 'RequestAnimationFrame'];
-                        window.cancelAnimationFrame = (window[prefix + 'CancelAnimationFrame'] || window[prefix + 'CancelRequestAnimationFrame']);
-                    }
-                    if (/iP(ad|hone|od).*OS 6/.test(window.navigator.userAgent) || // iOS6 is buggy
-                        !window.requestAnimationFrame || !window.cancelAnimationFrame) {
-                        lastTime = 0;
-                        window.requestAnimationFrame = function(callback) {
-                            var now = Date.now(),
-                            nextTime = Math.max(lastTime + 16, now);
-                            return setTimeout(function() { callback(lastTime = nextTime); }, nextTime - now);
-                        };
-                        window.cancelAnimationFrame = clearTimeout;
-                    }
-                }());
+        slide.style.width = (width += jump) + 'px';
+        past = time || 0;
+      },
+      timer = function timer() {
+        window.cancelAnimationFrame(handle);
+        width = 0;
+        reloadMaleglitch();
+        draw();
+      };
 
-                /**
-                 * Calculate the maximum possible height of the image
-                 */
-                (function() {
-                    var header = document.getElementsByTagName('h1')[0],
-                    footer = document.getElementsByTagName('footer')[0],
-        			h, f, d;
+      timer();
+      interval = setInterval(timer, duration * 1000);
+    }());
 
-                    h = parseFloat(window.getComputedStyle(header).height);
-                    h += parseFloat(window.getComputedStyle(header).marginTop);
-                    h += parseFloat(window.getComputedStyle(header).marginBottom);
+    // Google analytics
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-16179947-10']);
+    _gaq.push(['_setDomainName', 'maleglitch.net']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = 'http://www.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
 
-                    f = parseFloat(window.getComputedStyle(footer).height);
-                    f += parseFloat(window.getComputedStyle(footer).marginTop);
-                    f += parseFloat(window.getComputedStyle(footer).marginBottom);
-
-        			d = window.innerHeight - (h + f + 80);
-        			document.getElementsByTagName('img')[1].style.height = d + 'px';
-                }());
-            
-                /**
-                 * Function to change image source
-                 */
-                var reloadMaleglitch = function reloadMaleglitch() {
-                    var el = document.getElementsByTagName('img')[1]
-                    , path = el.src;
-                    path = path.replace(/\?q=.*$/, '?q=' + (new Date()).getTime());
-                    el.src = path;
-                }
-                , handle
-                , interval
-                , duration = 8
-                , width = 0
-                , past = 0
-                , step = (document.documentElement.clientWidth / duration)
-                , slide = document.getElementsByTagName('div')[1]
-                /**
-                 * Function to animate width of timer
-                 */
-                , draw = function draw(time) {
-                    var delta = (time || 0) - past
-                    , jump = (delta === 0) ? 0 : (step * (delta / 1000));
-                    handle = window.requestAnimationFrame(draw);
-                
-                    slide.style.width = (width += jump) + 'px';
-                    past = time || 0;
-                },
-                timer = function timer() {
-                    window.cancelAnimationFrame(handle);
-                    width = 0;
-                    reloadMaleglitch();
-                    draw();
-                };
-
-                timer();
-                interval = setInterval(timer, duration * 1000);
-            }());
-
-            // Google analytics
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-16179947-10']);
-            _gaq.push(['_setDomainName', 'maleglitch.net']);
-            _gaq.push(['_trackPageview']);
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = 'http://www.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-		</script>
-        
-	</body>
+</body>
 </html>
